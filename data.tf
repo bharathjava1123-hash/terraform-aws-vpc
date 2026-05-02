e6fd0bc8e8879828026062a878462d67f79e42ee
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-05-02 07:33:21 +0530

    terraform

--- data.tf
+++ data.tf
@@ -2,10 +2,10 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" {
+/* data "aws_vpc" "default" {
   default = true
 }
-
+ */
 data "aws_route_table" "main"{
   vpc_id = data.aws_vpc.default.id
   filter {
